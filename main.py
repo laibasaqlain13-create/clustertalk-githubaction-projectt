9e26c1ca25d938fca7f0406d9e5a0f178eea1d55
Laiba Saqlain <your-email@example.com>
2026-08-25 10:02:34 +0500

    Fix main.py execution loop

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import node.server
+import os
+import subprocess
+import sys
+
+if __name__ == "__main__":
+	# Ensure node/server.py runs continuously
+	subprocess.run([sys.executable, "node/server.py"])
